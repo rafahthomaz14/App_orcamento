fe754555433944795941ceaa818ef32302bf82ac
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2026-01-29 21:28:26 -0300

    adicionando barra navegacao

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import Constants from 'expo-constants';
 import { ScrollView, View } from "react-native";
 import { BtnAdicionar } from '../components/adicionar';
+import { BarraNav } from '../components/barraNav';
 import { CardNotificacao } from '../components/cardNotificacao';
 import { CardOrcamento } from '../components/cardOrcamento';
 import { Header } from '../components/header';
@@ -11,17 +12,26 @@ const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <ScrollView className='bg-gray-100'>
 
-      {/* adicionando tela inteira com um padding nas laterais de 5  */}
-      <View className="w-full px-5" style={{ marginTop: statusBarHeight + 8 }}>
-        <Header />
-        <BtnAdicionar />
-        <Orcamento />
-        <CardOrcamento/>
-        <Notificacao/>
-        <CardNotificacao/>
-      </View>
-    </ScrollView>
+    <View>
+      <ScrollView className='bg-gray-100'>
+
+        {/* adicionando tela inteira com um padding nas laterais de 5  */}
+        <View className="w-full px-5" style={{ marginTop: statusBarHeight + 8 }}>
+          <Header />
+          <BtnAdicionar />
+          <Orcamento />
+          <CardOrcamento />
+          <Notificacao />
+          <CardNotificacao />
+        </View>
+
+      </ScrollView>
+
+      <BarraNav />
+
+    </View>
+
+
   );
 }
